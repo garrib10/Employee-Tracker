f2be0fc7b4ef0c1f4c6216c10fc2cb4ca4f6222d
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-10-26 23:36:22 -0400

    added employee

--- server.js
+++ server.js
@@ -3,56 +3,93 @@ var inquirer = require("inquirer");
 var cTable = require("console.table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+  host: "localhost",
+  port: 3306,
   // Your username// 
-    user: "root",
-   // Add password at end// 
-    password: "",
-    database: "employee_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    start ();
-  });
+  user: "root",
+  // Add password at end// 
+  password: "",
+  database: "employee_db"
+});
 
-  function start (){
-    inquirer 
-     .prompt
-  }
-  
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
+});
+
+function start() {
+  inquirer
+    .prompt
+}
 
 
- 
-  function viewEmployee (){
 
-  }
 
-  function AddEmployee (){
+function viewEmployee() {
 
-  }
+}
 
-  function deleteEmployee (){
+function AddEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the employee's first name?"
+      },
+      {
+        type: 'input',
+        name: "last_name",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is the employee's role ID?",
+      },
+      {
+        type: "number",
+        name: "manager_id",
+        message: "What is the employee's manager ID?",
+      }
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT into employee SET?",
+        {
+          first_name: res.first_name,
+          last_name: res.last_name
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Employee has been succesfully added.");
+          start();
+        }
+      );
 
-  }
+    });
+}
 
-  function viewDepartment(){
+function deleteEmployee() {
 
-  }
-  
-  function addDepartment(){
+}
 
-  }
+function viewDepartment() {
 
-  function removeDepartment (){
+}
 
-  }
+function addDepartment() {
 
-  function updateRole (){
+}
 
-  }
+function removeDepartment() {
+
+}
+
+function updateRole() {
+
+}
 
 
   // TO DO LIST// 
@@ -60,4 +97,4 @@ var connection = mysql.createConnection({
   // What Do you want to do// 
   // View Department, Employee, Roles// 
   // Add Department, Employee, Roles// 
-  // Update Employee (delete employee, or update roles)// 
+  // Update Employee (delete employee, or update roles)/
